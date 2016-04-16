af8cf9b100f513e86b33c8e12237c905d5cb9f04
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2016-04-16 10:30:47 +0800

    [chore] Development version bumped to 0.0.2.dev0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 ## Setup:
 setup(name="django-lazydrf",
-      version="0.0.1",
+      version="0.0.2.dev0",
       packages=find_packages(),
       include_package_data=True,
       license="BSD License",
