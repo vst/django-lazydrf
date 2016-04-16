6a82501dee80406209ebb60b98c149d4654d4d34
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2016-04-16 10:07:28 +0800

    [release] Version bumped to 0.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 ## Setup:
 setup(name="django-lazydrf",
-      version="0.0.1.dev0",
+      version="0.0.1",
       packages=find_packages(),
       include_package_data=True,
       license="BSD License",
